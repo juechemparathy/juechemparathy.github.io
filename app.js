9406d93d666e045043641902ed98a3767e81f821
Jue Chemparathy <juchemp@amazon.com>
2025-11-08 13:36:34 -0800

    initial commit

--- app.js
+++ app.js
@@ -281,35 +281,26 @@ async function seedWeeklyData() {
       "Monday": {
         "6-8":  { p0: "Pickleball", p1: "Open Badminton" },
         "1-5":  { p0: "Open Badminton", p1: "Pickleball" },
-        "6-9":  { p0: "No Games", p1: "No Games" },
         "8-10": { p0: "Pickleball", p1: "Open Badminton" },
-        "9-11": { p0: "No Games", p1: "No Games" }
       },
       "Tuesday": {
         "6-8":  { p0: "Pickleball", p1: "Open Badminton" },
         "1-5":  { p0: "Pickleball", p1: "Open Badminton" },
-        "6-9":  { p0: "No Games", p1: "No Games" },
         "8-10": { p0: "Open Badminton", p1: "Pickleball" },
-        "9-11": { p0: "No Games", p1: "No Games" }
       },
       "Wednesday": {
         "6-8":  { p0: "Open Badminton", p1: "Pickleball" },
         "1-5":  { p0: "Pickleball", p1: "Open Badminton" },
-        "6-9":  { p0: "No Games", p1: "No Games" },
         "8-10": { p0: "Women’s Badminton", p1: "Open Badminton" },
-        "9-11": { p0: "No Games", p1: "No Games" }
       },
       "Thursday": {
         "6-8":  { p0: "Open Badminton", p1: "Pickleball" },
         "1-5":  { p0: "Open Badminton", p1: "Pickleball" },
-        "6-9":  { p0: "No Games", p1: "No Games" },
         "8-10": { p0: "Pickleball", p1: "Open Badminton" },
-        "9-11": { p0: "No Games", p1: "No Games" }
       },
       "Friday": {
         "6-8":  { p0: "Pickleball", p1: "Open Badminton" },
         "1-5":  { p0: "Pickleball", p1: "Open Badminton" },
-        "6-9":  { p0: "No Games", p1: "No Games" },
         "8-10": { p0: "Volleyball", p1: "Kids Games" },
         "9-11": { p0: "Volleyball", p1: "Kids Games" }
       },
@@ -317,15 +308,10 @@ async function seedWeeklyData() {
         "6-8":  { p0: "Volleyball", p1: "Pickleball" },
         "1-5":  { p0: "Kids Games", p1: "Pickleball" },
         "6-9":  { p0: "Basketball", p1: "Open Badminton" },
-        "8-10": { p0: "Basketball", p1: "No Games" },
-        "9-11": { p0: "No Games", p1: "No Games" }
       },
       "Sunday": {
-        "6-8":  { p0: "No Games", p1: "No Games" },
         "1-5":  { p0: "Volleyball", p1: "Pickleball" },
         "6-9":  { p0: "Pickleball", p1: "Open Badminton" },
-        "8-10": { p0: "No Games", p1: "No Games" },
-        "9-11": { p0: "Pickleball", p1: "No Games" }
       }
     };
 
