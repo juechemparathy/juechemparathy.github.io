c49d7aa719f274c73db7d89d8b20b2cc05639aa1
binoybt <binoybt@gmail.com>
2025-11-10 15:41:24 -0800

    Update app.js Changing Default

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const ADMIN_EMAILS = [
 ];
 
 let currentUser = null;
-let showAllSignups = true; // Admin toggle state - default to show all sign-ups
+let showAllSignups = false; // Admin toggle state - default to show Upcoming
 
 /*********************
  * UTILITY FUNCTIONS
